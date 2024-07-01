1282bcba89a42b4a5c778f12fdb49434bae4d6a9
BRANDON ALEXANDER JIMENEZ MEZA <bajimenez6@espe.edu.ec>
2024-07-01 17:05:55 -0500

    version 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/home/inicio/inicio.component';
+import { ReservarComponent } from './pages/reser/reservar/reservar.component';
+import { SobreComponent } from './pages/sec/sobre/sobre.component';
+import { LoginComponent } from './pages/inicio/login/login.component';
+import { JoinComponent } from './pages/crear/join/join.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home/inicio', component: InicioComponent},
+  {path: 'sec/sobre', component: SobreComponent},
+  {path: 'reser/reservar', component: ReservarComponent},
+  {path: 'crear/join',component:JoinComponent},
+  {path: 'inicio/login',component:LoginComponent},
+  {path: '',redirectTo: '/home/inicio', pathMatch:'full'},
+  {path: '**', redirectTo: '/home/inicio'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextoComponent } from './footer/texto/texto.component';
+import { ContenidoComponent } from './main/contenido/contenido.component';
+import { NavComponent } from './nav/nav/nav.component';
+import { InicioComponent } from './pages/home/inicio/inicio.component';
+import { ReservarComponent } from './pages/reser/reservar/reservar.component';
+import { SobreComponent } from './pages/sec/sobre/sobre.component';
+import { MainComponent } from './nosotros/main/main.component';
+import { FormulariosComponent } from './reser/formularios/formularios.component';
+import { LoginComponent } from './pages/inicio/login/login.component';
+import { JoinComponent } from './pages/crear/join/join.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
+  //declarar componentes
+  declarations: [AppComponent,TextoComponent,ContenidoComponent,NavComponent,InicioComponent,ReservarComponent,SobreComponent, MainComponent, FormulariosComponent, LoginComponent, JoinComponent],
+  imports: [ //modulos
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [], //servicios
   bootstrap: [AppComponent]
 })
 export class AppModule { }
